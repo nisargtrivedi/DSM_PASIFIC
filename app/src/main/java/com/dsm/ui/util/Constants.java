08c3b95021e16bd42e1e43514efb47071fe5175a
nisarg <nisarg.trivedi@vovance.com>
2021-08-17 08:54:44 +0530

    change label in search page

--- app/src/main/java/com/dsm/ui/util/Constants.java
+++ app/src/main/java/com/dsm/ui/util/Constants.java
@@ -11,9 +11,24 @@ import java.util.Date;
 public class Constants {
 
     //Development Credentials
-    public static String poolID = "ap-southeast-2_Li6mtJEjd";
-    public static String clientID = "7ssf34oku1ug890thv8dgpurvc";
-    public static String clientSecret = "10kd46djq7j5ge57f48u0ahjk7gibc0q1qglu77ctn0bu52hhck3";
+//    public static String poolID = "ap-southeast-2_Li6mtJEjd";
+//    public static String clientID = "7ssf34oku1ug890thv8dgpurvc";
+//    public static String clientSecret = "10kd46djq7j5ge57f48u0ahjk7gibc0q1qglu77ctn0bu52hhck3";
+//    public static Regions awsRegion = Regions.AP_SOUTHEAST_2;
+
+
+
+    //Live
+//    public static String poolID = "ap-southeast-2_tucUVac3M";
+//    public static String clientID = "4oqvni1krt6lsjk5cmmf3ec63d";
+//    public static String clientSecret = "1mprr91tq7cgd22c9l2ncj9kpbbbqr9nbimu88gdokkjb59o24sg";
+//    public static Regions awsRegion = Regions.AP_SOUTHEAST_2;
+
+
+    //test
+    public static String poolID = "ap-southeast-2_rqBSiLlwr";
+    public static String clientID = "5jdce2ikt6n8m9fd6uf8b02no9";
+    public static String clientSecret = "94slf3bp4vq38fje3eo1p370ohqffja0koca1kfvdcuedqf8rs9";
     public static Regions awsRegion = Regions.AP_SOUTHEAST_2;
 
 }
